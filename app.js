4323d543cf94b16a61220595e9f9c5589c66eec2
Alison <aarnett1@uncc.edu>
2022-02-22 21:13:56 -0500

    added code to app.js

---
+++ app.js
@@ -0,0 +1,77 @@
+// require modules
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const connectionRoutes = require('./routes/connectionRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+// create app
+const app = express();
+
+// configure app
+let port = 3000;
+let host = 'localhost';
+app.set('view engine', 'ejs');
+
+//connect to database
+mongoose.connect('mongodb://localhost:27017/project', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    app.listen(port, host, ()=>{
+        console.log('Server is running on port', port);
+    });
+})
+.catch(err => console.log(err.message));
+
+// mount middleware
+app.use(
+    session({
+        secret: "ajfeirf90aeu9eroejfoefj",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({mongoUrl: 'mongodb://localhost:27017/project'}),
+        cookie: {maxAge: 60*60*1000}
+        })
+);
+app.use(flash());
+
+app.use((req, res, next) => {
+    //console.log(req.session);
+    res.locals.user = req.session.user||null;
+    res.locals.errorMessages = req.flash('error');
+    res.locals.successMessages = req.flash('success');
+    next();
+});
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+
+// set up routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.use('/connections', connectionRoutes);
+app.use('/users', userRoutes);
+
+app.use((req, res, next) => {
+    let err = new Error('The server cannot locate ' + req.url);
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    if (!err.status) {
+        err.status = 500;
+        err.message = ("Internal Server Error");
+    }
+
+    res.status(err.status);
+    res.render('error', {error: err});
+});
