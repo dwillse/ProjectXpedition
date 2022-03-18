e0817b25956cb5b1e1bfadb7a5c203e00c09b5c2
Alison <aarnett1@uncc.edu>
2022-03-18 15:51:37 -0400

    edited app.js and worked on header/footer

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+//const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 //const connectionRoutes = require('./routes/connectionRoutes');
 //const userRoutes = require('./routes/userRoutes');
@@ -18,16 +18,16 @@ let host = 'localhost';
 app.set('view engine', 'ejs');
 
 //connect to database
-mongoose.connect('mongodb://localhost:27017/project', {useNewUrlParser: true, useUnifiedTopology: true})
+/*mongoose.connect('mongodb://localhost:27017/project', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
     app.listen(port, host, ()=>{
         console.log('Server is running on port', port);
     });
 })
-.catch(err => console.log(err.message));
+.catch(err => console.log(err.message));*/
 
 // mount middleware
-app.use(
+/*app.use(
     session({
         secret: "ajfeirf90aeu9eroejfoefj",
         resave: false,
@@ -36,15 +36,15 @@ app.use(
         cookie: {maxAge: 60*60*1000}
         })
 );
-app.use(flash());
+app.use(flash());*/
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     //console.log(req.session);
     res.locals.user = req.session.user||null;
     res.locals.errorMessages = req.flash('error');
     res.locals.successMessages = req.flash('success');
     next();
-});
+});*/
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
@@ -74,4 +74,8 @@ app.use((err, req, res, next) => {
 
     res.status(err.status);
     res.render('error', {error: err});
+});
+
+app.listen(port, host, () => {
+    console.log('Server is running on port', port);
 });
