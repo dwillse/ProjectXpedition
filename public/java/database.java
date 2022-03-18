b1c4c5ec895b88aebbb7d7d432d7adf47142a3aa
Dylan <dgoodm17@uncc.edu>
2022-03-18 19:08:25 -0400

    Fixing Java packages

---
+++ public/java/database.java
@@ -0,0 +1,11 @@
+public class Database {
+    String country;
+    String excursion;
+    Location location;
+    String website;
+    String description;
+    double reviews;
+    //couldn't remember if we decided on anything for tags so
+    //I'm just putting it here as a placeholder
+    String[] tags;
+}
