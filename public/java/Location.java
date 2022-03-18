7e051db87f65bdd03bf1a964aefe248d242a2bbc
Jason Freeman <jfreem67@uncc.edu>
2022-03-18 16:21:50 -0400

    Location

--- public/java/Location.java
+++ public/java/Location.java
@@ -1,5 +1,31 @@
-package public.java;
 
-public class Location {
+
+public class Location 
+{
+    private double latitude;
+    private double longitude;
+    
+    public Location(double la, double lo)
+    {
+        this.latitude = la;
+        this.longitude = lo;
+    }
+
+    private double getLatitude() {
+        return latitude;
+    }
+
+    private double getLongitude() {
+        return longitude;
+    }
+
+    private void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    private void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+    
     
 }
