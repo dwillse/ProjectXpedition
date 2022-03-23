b9ff920f84bb8d17a21cc7eb8541688e376961a9
jroth49 <jackroth49@gmail.com>
2022-03-23 19:34:54 -0400

    MERGE

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,4 +1,4 @@
-const { load } = require("nodemon/lib/config");
+
 
 window.onload = function hello() {
     let location = window.location.href;
@@ -7,11 +7,5 @@ window.onload = function hello() {
     location = location.charAt(0).toUpperCase() + location.slice(1);
     
     let pHeader = document.getElementById("pHeader");
-<<<<<<< HEAD
-    pHeader.innerHTML = location + "'s Preferences";
-}
-
-=======
     pHeader.innerHTML = location;
 }
->>>>>>> 0b2913f3110586bbe4fddd91fae1a7e48f125c3c
