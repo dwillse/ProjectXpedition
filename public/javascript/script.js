ebb922cffcc7c3630a4e893955bd171b532dd8b6
jroth49 <jackroth49@gmail.com>
2022-03-24 13:02:34 -0400

    updating script.js

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,13 +1,37 @@
 window.onload = function hello() {
     let location = window.location.href;
     location = location.substring(location.indexOf("=") + 1, location.length);
-
     location = location.charAt(0).toUpperCase() + location.slice(1);
-    
     let pHeader = document.getElementById("pHeader");
+    
+    if(window.location.href.includes("preferences")) {
+        pHeader.innerHTML = location;
+    }
 
-    pHeader.innerHTML = location;
+    if(window.location.href.includes("ratings")) {
+        let pref = window.location.href;
+        pref = pref.substring(pref.indexOf("pref"), pref.length);
+        let tags = pref.split("pref=", 10);
+        tags.shift();
+
+        for(var i = 0; i < tags.length; i++) {
+            if(tags[i].includes("&") || tags[i].includes("+")) {
+                tags[i] = tags[i].replace("&", "");
+                tags[i] = tags[i].replace("+", " ");
+            }
+        }
+
+        let pTags = document.getElementsByClassName("AttractionText");
+        
+        for(let i = 0; i < pTags.length; i++) {
+            pTags[i].innerHTML = tags[i];
+        }
+        
+
+
+    }
 }
+
 
 function selectedPref() {
     const btn = document.querySelector('#btn');
