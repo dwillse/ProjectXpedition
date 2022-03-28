c9d3dad1cdeb223dc27b9f49d133b9a5316288e6
Dylan <dgoodm17@uncc.edu>
2022-03-28 01:25:16 -0400

    Rerouted tag type to be String, added detail comments and tagged logic errors

--- public/java/Algorithm.java
+++ public/java/Algorithm.java
@@ -7,17 +7,49 @@ public class Algorithm {
 	public static ArrayList<Excursion> CallAlgorithim (UserPreference user, ArrayList<Excursion> exArray)
 	{
 
+		//finds best excursion for the user's highest rated tag
 		Excursion startingPoint = getStartingPoint(user, exArray);
-		ArrayList<Excursion> options = AllWithinFifty(startingPoint, exArray);
-        ArrayList<Excursion> finalList = new ArrayList<>();
-        ArrayList<Tag> tagsArray = new ArrayList<>();
-        tagsArray.add(startingPoint.getTags());
-        finalList.add(startingPoint);
-        
-        for (int i = 0; i < 4; i++) {
 
+		//finds every excursion within fifty miles of the startingpoint excursion
+		ArrayList<Excursion> options = AllWithinFifty(startingPoint, exArray);
+
+        ArrayList<Excursion> finalList = new ArrayList<>();
+        ArrayList<String> tagsArray = new ArrayList<>();
+		
+		/*Adds startingpoint's tag i.e. tag of first excursion recommended, to a list of tags that
+		* will not be recommended again
+		*/
+        tagsArray.add(startingPoint.getTag());
+
+		//adds the 'best excursion' found from getStartingPoint to the final list to be output at the end of the recommendation process
+        finalList.add(startingPoint);
+
+
+	
+        //Loop should run 4 times to recommend the remaining 4 excursions and put them into finalList
+        for (int i = 0; i < 4; i++) {
+			
+			/*alters loop based on the size of options where options is the list of all possible excursions
+			* able to be recommended
+			* This may not be the best way to do this, but I do think it works for now, it will only let the loop run
+			* for as long as we have options available
+			*/
+			if(options.size() == 3){
+				i++;
+			}else if(options.size() == 2){
+				i += 2;
+			}else if(options.size() == 1){
+				i += 3;
+			}else if(options.size() == 0){
+				break;
+			}
+
+			
+
+			//TODO logic here still needs to be fixed
             finalList.add(getNextExcursion(user, options, tagsArray));
-            tagsArray.add(finalList.get(i+1).getTags());
+
+            tagsArray.add(finalList.get(i+1).getTag());
         }
 
 
@@ -55,7 +87,7 @@ public class Algorithm {
 
 		Excursion startingPoint = exArray.get(0);
 		Boolean firstTime = true;
-		ArrayList<Tag> bestTags = user.getFiveStar();
+		ArrayList<String> bestTags = user.getFiveStar();
 		
 
 		//this simply goes down the list to make sure their highest ranked list of tags is used
@@ -89,7 +121,7 @@ public class Algorithm {
 			{
                 //If I have this correct, if bestTags string is equal to an excursion's tag
                 //it passes
-				if(bestTags.get(j).getTag1().equals(exArray.get(i).getTags().getTag1()))
+				if(bestTags.get(j).equals(exArray.get(i).getTag()))
 				{
                     //then if its the first time your startingPoint now becomes the excursion whose
                     //tag was equal to the bestTags
@@ -97,6 +129,7 @@ public class Algorithm {
 					{
 						startingPoint = exArray.get(i);
 						firstTime = !firstTime;
+						
 					}
                     
                     //then if the current excursion's reviews > than the startingPoint excursion's reviews
@@ -112,37 +145,48 @@ public class Algorithm {
 
 	}
 
-    private static Excursion getNextExcursion(UserPreference user, ArrayList<Excursion> exArray, ArrayList<Tag> tags)
+    private static Excursion getNextExcursion(UserPreference user, ArrayList<Excursion> exArray, ArrayList<String> tags)
 	{
-        ArrayList<Excursion> exArray2 = exArray;
-        UserPreference user2 = user;
+        
+      
 		Excursion startingPoint = exArray.get(0);
 		Boolean firstTime = true;
-		ArrayList<Tag> bestTags = user.getFiveStar();
-
+		ArrayList<String> bestTags = user.getFiveStar();
+/*
         for (int i = 0; i < exArray.size(); i++) {
             for (int j = 0; j < tags.size(); j++) {
-                if(exArray.get(i).getTags().getTag1().equals(tags.get(j).getTag1())){
-                    exArray2.remove(exArray.get(i));
+                if((exArray.get(i).getTags().getTag1().equals(tags.get(j).getTag1()))){
+                    //exArray2.add(exArray.get(i));
+					exArray2.remove(exArray.get(i));
                 }
             }
-        }
-    
+        }*/
+/*
+		for(int i = 0; i < exArray2.size(); i++)
+		{
+			System.out.println(exArray2.get(i).getExcursion());
+		}
+    */
+
+		//change contains function: does not produce correct result
         for (int i = 0; i < tags.size(); i++) {
+			//System.out.println("Tags: " + tags.get(i));
+			//System.out.println("Tag:" + user.getFiveStar().contains(tags.get(i)));
+			//System.out.println("Contains: " + user.getFiveStar().contains(tags.get(i)));
             if(user.getFiveStar().contains(tags.get(i))){
-                user2.getFiveStar().remove(tags.get(i));
+                user.getFiveStar().remove(tags.get(i));
             }
             if(user.getFourStar().contains(tags.get(i))){
-                user2.getFourStar().remove(tags.get(i));
+                user.getFourStar().remove(tags.get(i));
             }
             if(user.getThreeStar().contains(tags.get(i))){
-                user2.getThreeStar().remove(tags.get(i));
+                user.getThreeStar().remove(tags.get(i));
             }
             if(user.getTwoStar().contains(tags.get(i))){
-                user2.getTwoStar().remove(tags.get(i));
+                user.getTwoStar().remove(tags.get(i));
             }
             if(user.getOneStar().contains(tags.get(i))){
-                user2.getOneStar().remove(tags.get(i));
+                user.getOneStar().remove(tags.get(i));
             }
         }
 		
@@ -178,7 +222,7 @@ public class Algorithm {
 			{
                 //If I have this correct, if bestTags string is equal to an excursion's tag
                 //it passes
-				if(bestTags.get(j).getTag1().equals(exArray.get(i).getTags().getTag1()))
+				if(bestTags.get(j).equals(exArray.get(i).getTag()))
 				{
                     //then if its the first time your startingPoint now becomes the excursion whose
                     //tag was equal to the bestTags
