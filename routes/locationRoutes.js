f4fd64e5693337ad2f5a4b0582c0d75fea347621
Devin <dwillse@uncc.edu>
2022-04-14 20:18:56 -0400

    Added messages to the sign up page

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const controller = require('../controllers/locationController');
+const {isGuest, isLoggedIn} = require("../middlewares/auth");
+
 const router = express.Router();
 
 //Handles the choosing preferences page
-router.get("/preferences", controller.pref);
+router.get("/preferences", isLoggedIn, controller.pref);
 
 //handles the Rate preferences page
-router.get('/ratings' , controller.ratings);
+router.get('/ratings', controller.ratings);
 
 //Handles the details page
 router.get('/locations/:id' , controller.details);
