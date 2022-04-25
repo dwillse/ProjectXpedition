71f31807ec468ae15e316c28a5785f46cb98f0b0
Alison <aarnett1@uncc.edu>
2022-04-25 15:13:53 -0400

    country and preferences stored in database in one entry

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,15 +1,13 @@
 const model = require('../models/location');
 const userModel = require("../models/user");
 const preferenceModel = require("../models/preference");
+const prefs = new preferenceModel();
 
 
+// post for selecting country and redirecting to choosePref
 exports.country = (req, res) => {
     let user = req.session.user;
-    //let id = req.param.id;
-    let country = new preferenceModel();
-    country.country = req.body.location;
-    //const chosenCountry = country.country;
-    country.save();
+    prefs.country = req.body.location;
     Promise.all([userModel.findById(user)])
     .then(results => {
         const [user] = results;
@@ -18,34 +16,29 @@ exports.country = (req, res) => {
     .catch(err=>next(err));
 };
 
+// get for redirecting to choosePref
 exports.pref = (req, res, next) => {
-    console.log(req.body.location);
     res.render('./location/choosePref');
 }
 
+// get for redirecting to ratePref
 exports.ratings = (req, res) => {
     res.render('./location/ratePref');
 };
 
+// get for redirecting to results
 exports.results = (req, res) => {
     res.render('./location/results');
 }
 
+// post for chosen and rated preferences and redirecting to results page
 exports.itinerary = (req, res, next) => {   
-    let id = req.param.id;
-    let location = model.findById(id);
-    if(location) {
-        let prefs = new preferenceModel();
-        prefs.chosen = req.body;
-        prefs.save();
-        res.render('./location/results');
-    } else {
-        let err = new Error('Cannot find that location' + id);
-        err.status = 404;
-        next(err);
-    }
+    prefs.chosen = req.body;
+    prefs.save();
+    res.render('./location/results');
 };
 
+// redirects to details page and inserts info into page
 exports.details = (req, res, next)=> {
     let id = req.params.id;
     let location = model.findById(id);
